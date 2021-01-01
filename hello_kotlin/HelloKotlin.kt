187f47cab87dc5bbcc2d12cb9b5a1d4e4a8bdf46
AnthonyJo <joseongkwang@gmail.com>
2021-01-01 19:35:13 +0900

    init project

---
+++ hello_kotlin/HelloKotlin.kt
@@ -0,0 +1,13 @@
+
+
+class test() {
+
+  fun main () {
+    val a = 10
+
+    println("Hello Kotlin")
+    println("a = " + a)
+  }
+}
+
+//vs 코드에서 코틀린 패키지 경로를 어떻게 써야 하는가?
---
+++ variable/Variable.kt
@@ -0,0 +1,12 @@
+package com.KotlinExample.Variable
+
+class Variable() {
+
+  fun main () {
+    val name: String = "Anthony Jo"
+    val age: Int = 33
+
+    println("name: " + name)
+    println("age:  " + age)
+  }
+}
