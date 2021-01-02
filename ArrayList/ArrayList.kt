ee5418f94b9cda87fd3314f145801743d5d91bc2
AnthonyJo <joseongkwang@gmail.com>
2021-01-02 21:21:18 +0900

    Array List

---
+++ ArrayList/ArrayList.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val ARRAY_SIZE = 5
+
+    val array1 = arrayListOf(1, 2, 3, 4, 5)     //  [1 ,2 ,3, 4, 5] 
+//    val array2 = ArrayList(ARRAY_SIZE, {0})
+//    val array3 = ArrayList(ARRAY_SIZE, {i -> i})
+
+    
+    println("array1[] = ${array1}")
+//    println("array2[] = ${array2}")
+//    println("array3[] = ${array3}")
+}
